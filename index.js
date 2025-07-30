e569b9b464afa8f2d6702ccdecaa78a7971298db
Vibhutibahen Sangani <sanganivibhuti@gmail.com>
2025-07-30 18:20:20 +0200

    made Rock-Paper-Scissors

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log(process.argv);
+const args = process.argv.slice(2);
+const Player1 = args[0];
+console.log(`You chose ${Player1}`);
+
+const value = ["Rock", "Paper", "Scissors"];
+
+if (Player1 !== "Rock" && Player1 !== "Paper" && Player1 !== "Scissors") {
+  console.log("Please write Any option from Rock Paper or Scissors");
+  return;
+}
+const randomInput = Math.floor(Math.random() * value.length);
+const Player2 = value[randomInput];
+console.log(`Player2 chose ${Player2}`);
+
+if (
+  (Player1 === "Rock" && Player2 === "Scissors") ||
+  (Player1 === "Scissors" && Player2 === "Paper") ||
+  (Player1 === "Paper" && Player2 === "Rock")
+) {
+  console.log("............You Won the game!............");
+  return;
+} else if (
+  (Player2 === "Rock" && Player1 === "Scissors") ||
+  (Player2 === "Scissors" && Player1 === "Paper") ||
+  (Player2 === "Paper" && Player1 === "Rock")
+) {
+  console.log("............You Lost the game!............");
+} else if (Player1 === Player2) {
+  console.log("............it’s a draw^..^!.............");
+}
