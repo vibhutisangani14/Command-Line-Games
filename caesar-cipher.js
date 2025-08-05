015d22932d9a3c20a3b437148e9f6e0b5a2ec083
Vibhutibahen Sangani <sanganivibhuti@gmail.com>
2025-08-05 14:56:16 +0200

    made caesar-cipher game

---
+++ caesar-cipher.js
@@ -0,0 +1,84 @@
+console.log(process.argv);
+const alphabets = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const userInput = process.argv.slice(2);
+const inputString = userInput[0];
+const inputArray = inputString.split("");
+console.log(inputArray);
+
+const position = userInput[1];
+const inputPosition = parseInt(userInput[1]);
+console.log("user Input :- " + inputPosition);
+
+function isOnlyLetters(str) {
+  return !/\d/.test(str);
+}
+// console.log(inputString);
+
+if (inputPosition > 26 || inputPosition < -26) {
+  console.log("Please enter position between -26 to 26");
+  return;
+} else if (!isOnlyLetters(inputString)) {
+  console.log("Please enter first input only as a String");
+  return;
+} else if (isOnlyLetters(position)) {
+  console.log("Please write the second input as a number");
+  return;
+}
+
+let newArray = [];
+
+for (let index = 0; index < inputArray.length; index++) {
+  let shiftedLetter;
+  const element = inputArray[index];
+
+  if (element !== " ") {
+    const letter = element.toLowerCase();
+    const actualPosition = alphabets.indexOf(letter) + 1;
+    let pos = actualPosition + inputPosition;
+    if (pos > alphabets.length) {
+      pos = alphabets.length - actualPosition;
+      const finalPos = inputPosition - pos;
+      shiftedLetter = alphabets[finalPos - 1];
+    } else if (pos < 1) {
+      pos = alphabets.length + pos;
+      shiftedLetter = alphabets[pos - 1];
+    } else {
+      shiftedLetter = alphabets[pos - 1];
+    }
+    newArray[index] = shiftedLetter;
+  } else {
+    newArray[index] = element;
+    console.log(element);
+  }
+}
+console.log("Input :- " + inputString);
+
+console.log("Output :- " + newArray.join(""));
